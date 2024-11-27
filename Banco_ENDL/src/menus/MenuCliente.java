410086639cdf2f78384eb7debe9f31f72d572d03
Leonardo Lopez <lopezmezaleonardo553@gmail.com>
2024-11-27 08:09:28 -0600

    Empezar aqui

--- Banco_ENDL/src/menus/MenuCliente.java
+++ Banco_ENDL/src/menus/MenuCliente.java
@@ -11,48 +11,58 @@ public class MenuCliente {
     public ArrayList<Cliente> clientes = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
 
-    public void mostrarMenuCliente() {
+
+    public int mostrarDatos() {
         for (Cliente cliente : clientes) {
             if (cliente.getSaldo() < 30000) {
-                int opcion = 6;
-
-                while (opcion != 4) {
-                    System.out.println("""
-                            1.- Hacer deposito
-                            2.- Hacer retiro
-                            3.- Saldo de cuenta
-                            4.- Salir""");
-                    opcion = sc.nextInt();
-
-                    switch (opcion) {
-                        case 1:
-                            System.out.print("Digite el numero de la tarjeta:");
-                            double numeroTarjeta = sc.nextDouble();
-
-                            System.out.print("Digite la cantidad a depositar: ");
-                            double deposito = sc.nextDouble();
-
-                            break;
-                        case 2:
-                            System.out.print("Digite la cantidad a retirar: ");
-                            double retiro = sc.nextDouble();
-                            break;
-                        case 3:
-                            System.out.println("***** SALDO DE LA CUENTA *****");
-                            break;
-                        case 4:
-                            break;
-                    }
-                }
+                System.out.println("""
+                        \n***BIENVENIDO***
+                        1.- Hacer deposito
+                        2.- Hacer retiro
+                        3.- Saldo de cuenta
+                        4.- Salir""");
+                int opcion = sc.nextInt();
+                return opcion;
             } else {
                 System.out.println("""
                         1.- Hacer deposito
                         2.- Hacer retiro
                         3.- Saldo de cuenta
-                        5.- Solicitar tarjeta de debito
+                        5.- Solicitar tarjeta de credito
                         6.- Hacer compra
                         4.- Salir""");
+                int opcion = sc.nextInt();
+                return opcion;
             }
+        }
+
+    }
+
+    public void procesarDatos(int opcion) {
+
+        switch (opcion) {
+            case 1:
+                System.out.print("Digite el numero de la tarjeta:");
+                double numeroTarjeta = sc.nextDouble();
+
+                System.out.print("Digite la cantidad a depositar: ");
+                double deposito = sc.nextDouble();
+
+                break;
+            case 2:
+                System.out.print("Digite la cantidad a retirar: ");
+                double retiro = sc.nextDouble();
+                break;
+            case 3:
+                System.out.println("***** SALDO DE LA CUENTA *****");
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
         }
     }
 }
+
---
+++ Banco_ENDL/src/tarjetas/Debito.java
@@ -0,0 +1,9 @@
+package tarjetas;
+
+import java.time.LocalDate;
+
+public class Debito extends Tarjeta {
+    public Debito(String titular, String numeroTarjeta, String fechaCreacion, double saldo, int cvv, String clabeInter, LocalDate fechaVencimiento, String ultimoMovimiento) {
+        super(titular, numeroTarjeta, fechaCreacion, saldo, cvv, clabeInter, fechaVencimiento, ultimoMovimiento);
+    }
+}
